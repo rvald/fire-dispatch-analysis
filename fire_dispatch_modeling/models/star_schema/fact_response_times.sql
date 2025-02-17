6bd0cda984dd3eed2f2aa54dd740b4d13f936370
rvald <rsvald84@gmail.com>
2025-02-16 20:33:07 -0500

    created fact_response_times dbt model

---
+++ fire_dispatch_modeling/models/star_schema/fact_response_times.sql
@@ -0,0 +1,33 @@
+
+-- "What is the average response time from the 
+-- dispatch point to the on-scene arrival across different 
+-- geographies and timeframes?"
+
+select 
+    {{ dbt_utils.generate_surrogate_key([
+        'STARFIRE_INCIDENT_ID', 
+        'INCIDENT_DATETIME'
+    ]) }} AS fact_response_time_key,
+
+    {{ dbt_utils.generate_surrogate_key([
+        'INCIDENT_BOROUGH', 
+        'ZIPCODE', 
+        'POLICEPRECINCT', 
+        'CITYCOUNCILDISTRICT', 
+        'COMMUNITYDISTRICT', 
+        'COMMUNITYSCHOOLDISTRICT', 
+        'CONGRESSIONALDISTRICT'
+    ]) }} AS location_key,
+
+    {{ dbt_utils.generate_surrogate_key([
+        'ALARM_BOX_BOROUGH', 
+        'ALARM_BOX_NUMBER', 
+        'ALARM_BOX_LOCATION'
+    ]) }} AS alarm_key,
+
+    INCIDENT_DATETIME as incident_datetime,
+    FIRST_ASSIGNMENT_DATETIME as first_assignment_datetime,
+    FIRST_ON_SCENE_DATETIME as first_on_scene_datetime
+from 
+    {{ var("source_schema") }}.biglake_dispatch_iceberg 
+    
